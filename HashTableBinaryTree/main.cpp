2fcbe0aa742edad209338f08c1cbfd303049a978
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-07-18 22:29:39 -0300

    Main criado

--- HashTableBinaryTree/main.cpp
+++ HashTableBinaryTree/main.cpp
@@ -3,17 +3,34 @@
 #include <regex>
 #include <fstream>
 #include "Utils.h"
+#include "HashTable.h"
 
 using namespace std;
+
+Email GetEmailFromString(string message) {
+
+}
 
 int main()
 {
 	string line;
 	ifstream myfile;
-	int melhoriaAlg = 4;
+
+	int u, e, n, m;
+
 	myfile.open("entrada_1.txt");
+	myfile >> m;
+
+	HashTable* hash = new HashTable(m);
+
 	while (getline(myfile, line))
 	{
+		if (line[0] == 'E')
+			hash->InsereEmail(&GetEmailFromString(line));
+		else if (line[0] == 'C')
+			break;
+		else
+			break;
 	}
 	myfile.close();
 }
