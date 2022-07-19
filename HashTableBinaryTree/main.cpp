f78dc36306f37c47d3cfdfe153cbacb5f9e4b681
Gabriel de Paula <gabrieldepaula007@gmail.com>
2022-07-18 22:18:04 -0300

    Add: Testes

--- HashTableBinaryTree/main.cpp
+++ HashTableBinaryTree/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <string>
+#include <regex>
+#include <fstream>
+#include "Utils.h"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	string line;
+	ifstream myfile;
+	int melhoriaAlg = 4;
+	myfile.open("entrada_1.txt");
+	while (getline(myfile, line))
+	{
+	}
+	myfile.close();
 }
